332e665a0db42c4d863751299fec6bcf3dc8c9ec
lightyn <pnelson88@gmail.com>
2017-07-20 02:10:45 -0400

    Refactored media queries to support dual pane layout in full size web browsers. Scripted active pane toggle in mobile.

--- source/clientBrowser/scripts/accordion.js
+++ source/clientBrowser/scripts/accordion.js
@@ -1,16 +1,25 @@
-  $( "#accordion" )
-    .accordion({
-      header: "> div > h3", collapsible: true
-    })
-    .sortable({
-      axis: "y",
-      handle: "h3",
-      stop: function( event, ui ) {
-        // IE doesn't register the blur when sorting
-        // so trigger focusout handlers to remove .ui-state-focus
-        ui.item.children( "h3" ).triggerHandler( "focusout" );
+//const accordions = document.getElementsByClassName("accordion");
+//let i;
+//for (i=0;i=accordions.length;i++) {
+  $(".accordion").accordion({
+    header: "> div > h3",
+    collapsible: true
+  })
+  .sortable({
+    axis: "y",
+    handle: "h3",
+    stop: function(event, ui) {
+      // IE doesn't register the blur when sorting
+      // so trigger focusout handlers to remove .ui-state-focus
+      ui.item.children("h3").triggerHandler("focusout");
 
-        // Refresh accordion to handle new order
-        $( this ).accordion( "refresh" );
-      }
-    });
+      // Refresh accordion to handle new order
+      $(this).accordion("refresh");
+    }
+  });
+//}
+
+//TODO: Rebuild accordion script without jqueryui
+/*
+  let accordion = document.getElementsByClassName("accordion");
+  accordion.forEach() */
